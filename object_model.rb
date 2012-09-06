86b46aaf706fe165eb1c27db9b59d2d491aaf137
huangcd @Y430 <huangcd.thu@gmail.com>
2012-09-06 21:18:23 +0800

    test push

--- object_model.rb
+++ object_model.rb
@@ -3,8 +3,18 @@ class ObjectModel
     "I'm only in ObjectModel"
   end
 
+  class << self
+    def another_class_method
+      "I'm a strange class method in ObjectModel"
+    end
+  end
+
   def instance_method
     @var = 1
     "I'm in the instance of ObjectModel"
   end
+end
+
+def ObjectModel.third_class_method
+  "I'm still a class method of ObjectModel"
 end
